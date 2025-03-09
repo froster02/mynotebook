88dbe254131c0a83677922ac39f9a26b555a48f3
Arush Naudiyal <arushnaudiyal@gmail.com>
2025-03-09 23:05:46 +0530

    feat: update login route to retrieve and return user details after authentication

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -152,8 +152,9 @@ router.post('/createuser', [
 router.post('/login', fetchuser, async (req, res) => {
 
     try {
-        const userId = "todo";
+        const userId = req.user.id;
         const user = await User.findById(userId).select("-password");
+        res.send(user);
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Internal Server Error");
