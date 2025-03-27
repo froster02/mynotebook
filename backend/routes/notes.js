3616c546cdeaa93111fb884e9252218cc2841a33
Arush Naudiyal <arushnaudiyal@gmail.com>
2025-03-27 23:43:41 +0530

    feat: add addnote route for creating new notes with validation and error handling

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,14 +1,47 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const { body, validationResult } = require('express-validator');
 const router = express.Router();
 var fetchuser = require('../middleware/fetchuser');
-const Notes = require('../models/Notes');
+const Note = require('../models/Notes');
 
 //Router 1 : Get all the notes using : GET "/api/notes/fetchallnotes". Login required
 router.get('/fetchallnotes', fetchuser, async (req, res) => {
-    const notes = await Notes.find({ user: req.user.id });
-    res.json(notes);
+    try {
+        const notes = await Note.find({ user: req.user.id });
+        res.json(notes)
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
 });
+
+//Router 2 : Add a new notes using : GET "/api/notes/addnote". Login required
+router.post('/addnote', fetchuser, [
+    body('title', 'Enter a valid title').isLength({ min: 3 }),
+    body('description', 'Description must be atleast 5 characters').isLength({ min: 5 }),
+], async (req, res) => {
+
+    try {
+        //If there are errors, return Bad request and the errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ success: false, error: errors.array() });
+        }
+        const { title, description, tag } = req.body;
+        //Creating a new note
+        const note = new Note({
+            title, description, tag, user: req.user.id
+        });
+        const savedNote = await note.save();
+        res.json(savedNote);
+    }
+    catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
 
 module.exports = router;
