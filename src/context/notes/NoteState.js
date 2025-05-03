60ec7f6c25386010a86de063ea0b806a17b6f17d
Arush Naudiyal <arushnaudiyal@gmail.com>
2025-05-04 00:01:38 +0530

    feat: add initial notes state to NoteState context provider

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -26,8 +26,34 @@ import NoteContext from "./notesContext";
  */
 const NoteState = (props) => {
 
+    const notes =
+        [
+            {
+                "_id": "67e979265139f37fde095803",
+                "user": "67cddc2866828567a72743f8",
+                "title": "My Title",
+                "description": "Morning! Hit the gym",
+                "tag": "personal",
+                "date": "2025-03-30T17:02:30.725Z",
+                "createdAt": "2025-03-30T17:02:30.726Z",
+                "updatedAt": "2025-03-30T17:02:30.726Z",
+                "__v": 0
+            },
+            {
+                "_id": "67e979275139f37fde095805",
+                "user": "67cddc2866828567a72743f8",
+                "title": "My Title",
+                "description": "Morning! Hit the gym",
+                "tag": "personal",
+                "date": "2025-03-30T17:02:31.861Z",
+                "createdAt": "2025-03-30T17:02:31.862Z",
+                "updatedAt": "2025-03-30T17:02:31.862Z",
+                "__v": 0
+            }
+        ];
+
     return (
-        <NoteContext.Provider value={{}}>
+        <NoteContext.Provider value={{ notes }}>
             {props.children}
         </NoteContext.Provider>
     )
