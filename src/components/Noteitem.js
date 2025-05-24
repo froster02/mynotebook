019e4ebd35e8f888245e3b3a6650007f0ca59c2c
Arush Naudiyal <arushnaudiyal@gmail.com>
2025-05-24 17:59:39 +0530

    refactor: improve layout and styling of Noteitem component for better readability

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -8,17 +8,23 @@ const Noteitem = (props) => {
 
     return (
         <div className="col-md-3">
-            <div className="card my-3">
-                <div className="card-body">
-                    <div className='d-flex align-items-center'>
-                        <h5 className="card-title">{note.title || 'Untitled'}</h5>
-                        <p className="card-text">{note.description || 'No description'}</p>
-                        <i className="far fa-trash-alt mx-2" onClick={() => { deleteNote(note._id) }}></i>
-                        <i className="far fa-edit mx-2" onClick={() => { updateNote(note) }}></i>
+            <div className="card my-3" style={{ minHeight: '20px' }}>
+                <div className="card-body d-flex flex-column">
+                    <div className='d-flex justify-content-between align-items-start'>
+                        <h5 className="card-title" style={{ marginBottom: '15px', overflow: 'hidden', textOverflow: 'ellipsis' }}>
+                            {note.title || 'Untitled'}
+                        </h5>
+                        <div>
+                            <i className="far fa-trash-alt mx-2" style={{ cursor: 'pointer', color: '#dc3545' }} onClick={() => { deleteNote(note._id) }}></i>
+                            <i className="far fa-edit mx-2" style={{ cursor: 'pointer', color: '#0d6efd' }} onClick={() => { updateNote(note) }}></i>
+                        </div>
                     </div>
+                    <p className="card-text" style={{ flex: '1', overflow: 'hidden', textOverflow: 'ellipsis' }}>
+                        {note.description || 'No description'}
+                    </p>
                 </div>
             </div>
-        </div >
+        </div>
     )
 }
 
