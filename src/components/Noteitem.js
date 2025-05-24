15515e727b23e9e6d8e173001074c653e78ec8c3
Arush Naudiyal <arushnaudiyal@gmail.com>
2025-05-24 16:53:21 +0530

    feat: implement note editing functionality with modal for updating note details

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -3,17 +3,8 @@ import noteContext from '../context/notes/notesContext';
 
 const Noteitem = (props) => {
     const context = useContext(noteContext);
-    const { note } = props;
     const { deleteNote } = context;
-
-    const handleDelete = async () => {
-        try {
-            await deleteNote(note._id);
-        } catch (error) {
-            console.error('Error deleting note:', error);
-            // TODO: Show error to user via alert/toast
-        }
-    };
+    const { note, updateNote } = props;
 
     return (
         <div className="col-md-3">
@@ -22,12 +13,12 @@ const Noteitem = (props) => {
                     <div className='d-flex align-items-center'>
                         <h5 className="card-title">{note.title || 'Untitled'}</h5>
                         <p className="card-text">{note.description || 'No description'}</p>
-                        <i className="far fa-trash-alt mx-2" onClick={handleDelete}></i>
-                        <i className="far fa-edit mx-2"></i>
+                        <i className="far fa-trash-alt mx-2" onClick={() => { deleteNote(note._id) }}></i>
+                        <i className="far fa-edit mx-2" onClick={() => { updateNote(note) }}></i>
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
